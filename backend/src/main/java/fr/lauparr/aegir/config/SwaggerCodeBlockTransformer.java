088067ea0640ee8d0279267b65f71db241736d2e
lauparr <laurent.parrot78@gmail.com>
2022-08-08 21:14:10 +0200

    fix: cannot access css resource when packaged

--- backend/src/main/java/fr/lauparr/aegir/config/SwaggerCodeBlockTransformer.java
+++ backend/src/main/java/fr/lauparr/aegir/config/SwaggerCodeBlockTransformer.java
@@ -1,25 +1,29 @@
 package fr.lauparr.aegir.config;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springdoc.core.SwaggerUiConfigProperties;
 import org.springdoc.core.SwaggerUiOAuthProperties;
 import org.springdoc.core.providers.ObjectMapperProvider;
 import org.springdoc.webmvc.ui.SwaggerIndexPageTransformer;
 import org.springdoc.webmvc.ui.SwaggerWelcomeCommon;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.resource.ResourceTransformerChain;
 import org.springframework.web.servlet.resource.TransformedResource;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @Component
 public class SwaggerCodeBlockTransformer extends SwaggerIndexPageTransformer {
+
+  @Autowired
+  ResourceLoader resourceLoader;
 
   public SwaggerCodeBlockTransformer(SwaggerUiConfigProperties swaggerUiConfig, SwaggerUiOAuthProperties swaggerUiOAuthProperties, SwaggerUiConfigParameters swaggerUiConfigParameters, SwaggerWelcomeCommon swaggerWelcomeCommon, ObjectMapperProvider objectMapperProvider) {
     super(swaggerUiConfig, swaggerUiOAuthProperties, swaggerUiConfigParameters, swaggerWelcomeCommon, objectMapperProvider);
@@ -28,9 +32,13 @@ public class SwaggerCodeBlockTransformer extends SwaggerIndexPageTransformer {
   @Override
   public Resource transform(HttpServletRequest request, Resource resource, ResourceTransformerChain transformer) throws IOException {
     if (resource.toString().contains("swagger-ui.css")) {
-      File file = ResourceUtils.getFile("classpath:swagger-ui.min.css");
-      final byte[] transformedContent = FileUtils.readFileToString(file, StandardCharsets.UTF_8).getBytes();
-      return new TransformedResource(resource, transformedContent);
+      try {
+        Resource customCssResource = resourceLoader.getResource("classpath:swagger-ui.min.css");
+        final byte[] transformedContent = IOUtils.toString(customCssResource.getInputStream(), StandardCharsets.UTF_8).getBytes();
+        return new TransformedResource(resource, transformedContent);
+      } catch (FileNotFoundException e) {
+        return super.transform(request, resource, transformer);
+      }
     }
     return super.transform(request, resource, transformer);
   }
